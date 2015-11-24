379455d248e26e475060613174a8ed8876659c45
Xxdzs <sid.xxdzs@gmail.com>
2015-11-24 13:59:38 +0100

    +Header, + 1 fun, MAJ 1 fun

--- Libft/libft.h
+++ Libft/libft.h
@@ -1,18 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/24 13:32:54 by angagnie          #+#    #+#             */
-/*   Updated: 2015/11/24 13:49:22 by angagnie         ###   ########.fr       */
+/*   Created: 2015/11/24 13:50:45 by angagnie          #+#    #+#             */
+/*   Updated: 2015/11/24 13:58:19 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_isalnum(int c)
-{
-	return (('a' <= c && c <= 'z')
-			|| ('A' <= c && c <= 'Z')
-			|| ('0' <= c && c <= '9'));
-}
+#include <string.h>
+
+void    ft_bzero(void *s, size_t n);
+int     ft_isalnum(int c);
+int     ft_isalpha(int c);
+int     ft_isascii(int c);
+int     ft_iscntrl(int c);
+int     ft_isdigit(int c);
+int     ft_isprint(int c);
+void    *ft_memset(void *b, int c, size_t len);
+char    *ft_stpcpy(char *dst, const char *src);
+size_t	ft_strlen(char const *str);
+int     ft_tolower(int c);
+int     ft_toupper(int c);
