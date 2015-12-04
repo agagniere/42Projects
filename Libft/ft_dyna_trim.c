93adb1559084e67fcce02b8d448ac1f98b5a44b2
Xxdzs <sid.xxdzs@gmail.com>
2015-12-04 23:06:38 +0100

    Updated

--- Libft/ft_dyna_trim.c
+++ Libft/ft_dyna_trim.c
@@ -6,15 +6,18 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 18:17:19 by angagnie          #+#    #+#             */
-/*   Updated: 2015/12/04 18:24:43 by angagnie         ###   ########.fr       */
+/*   Updated: 2015/12/04 21:18:44 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_dyna_trim(t_dyna *td)
 {
 	void	*new_data;
 
-	if (!(new_data = malloc(td->chunk_size * td_chunck)))
+	if (!(new_data = malloc(td->chunk_size * td->chunck_count)))
 		return (1);
-	ft_mem
+	ft_memcpy(new_data, td->data, td->chunck_count * td->chunck_size);
+	return (0);
 }
