89b7b96dd246ca18a8ff01275eededdf274c9d1a
Antoine Gagniere <sid.xxdzs@gmail.com>
2015-12-07 16:51:28 +0100

    Update ft_dyna_trim.c

--- Libft/ft_dyna_trim.c
+++ Libft/ft_dyna_trim.c
@@ -13,6 +13,10 @@
 #include "libft.h"
 #include <stdlib.h>
 
+/*
+** http://www.cplusplus.com/reference/vector/vector/shrink_to_fit/
+*/
+
 int	ft_dyna_trim(t_dyna *td)
 {
 	void	*new_data;
@@ -22,8 +26,8 @@ int	ft_dyna_trim(t_dyna *td)
 		if (!(new_data = malloc(td->chunck_size * td->chunck_count)))
 			return (1);
 		ft_memcpy(new_data, td->data, td->chunck_count * td->chunck_size);
-		td->chunck_max = td->chunck_count;
 		free(td->data);
+		td->chunck_max = td->chunck_count;
 		td->data = new_data;
 	}
 	return (0);
