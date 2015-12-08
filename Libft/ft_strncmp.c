8f63a5929fed95b3792e2d9a1378071795792d87
Xxdzs <sid.xxdzs@gmail.com>
2015-12-08 20:05:35 +0100

    mces

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 17:09:41 by angagnie          #+#    #+#             */
-/*   Updated: 2015/12/08 19:48:01 by angagnie         ###   ########.fr       */
+/*   Updated: 2015/12/08 20:04:46 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (n == 0)
-		return (0);
-	while (*s1 == *s2 && *s1 != 0 && n-- > 0)
+	while (n-- > 0)
 	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		else if (*s1 == '\0')
+			return (0);
 		s1++;
 		s2++;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return (0);
 }
--- Libft/ft_strrchr.c
+++ Libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 17:38:45 by angagnie          #+#    #+#             */
-/*   Updated: 2015/12/08 19:55:33 by angagnie         ###   ########.fr       */
+/*   Updated: 2015/12/08 20:00:11 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ char	*ft_strrchr(const char *s, int c)
 {
 	char const	*p;
 
+
 	p = s;
 	while (*p != '\0')
 		p++;
 	while (*(const unsigned char *)p != c && p >= s)
 		p--;
-	return (*p == c ? (char *)p : NULL);
+	return (*(const unsigned char *)p == c ? (char *)p : NULL);
 }
