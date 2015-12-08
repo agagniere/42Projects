024f792f2e62c03ceb0480193f97a20c3df6ba55
Xxdzs <sid.xxdzs@gmail.com>
2015-12-08 20:19:42 +0100

    FINAL

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 16:28:12 by angagnie          #+#    #+#             */
-/*   Updated: 2015/12/07 18:34:14 by angagnie         ###   ########.fr       */
+/*   Updated: 2015/12/08 20:19:16 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,5 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list	*ans;
-	t_list	*ptr;
-
-	ptr = f(lst);
-	ans = ft_lstnew(ptr->content, ptr->content_size);
-	while ((lst = lst->next) != NULL)
-	{
-		ptr = f(lst);
-		ft_lstpush(&ans, ft_lstnew(ptr->content, ptr->content_size));
-	}
-	return (ans);
+	return (ft_lstmap_rec(lst, f));
 }
--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 15:04:21 by angagnie          #+#    #+#             */
-/*   Updated: 2015/12/07 20:33:13 by angagnie         ###   ########.fr       */
+/*   Updated: 2015/12/08 19:42:51 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 
 	ans = (t_list *)malloc(sizeof(t_list));
 	ft_bzero(ans, sizeof(t_list));
-	ans->content = (void *)content;
 	if (content != NULL)
+	{
 		ans->content_size = content_size;
+		ans->content = malloc(content_size);
+		ft_memcpy(ans->content, content, content_size);
+	}
 	ans->next = NULL;
 	return (ans);
 }
