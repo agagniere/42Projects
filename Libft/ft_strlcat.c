85cf19bd28e376d2bae2c089c6adeed215c022b9
Xxdzs <sid.xxdzs@gmail.com>
2015-12-08 18:04:52 +0100

    Oops

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 14:12:37 by angagnie          #+#    #+#             */
-/*   Updated: 2015/12/08 18:00:41 by angagnie         ###   ########.fr       */
+/*   Updated: 2015/12/08 18:04:23 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst += dst_len;
 	while ((++dst_len) < size && *src != '\0')
 		*dst++ = *src++;
-	*dst = '0';
+	*dst = '\0';
 	return (ret);
 }
