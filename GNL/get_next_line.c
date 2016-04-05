5c5eb7c4008f8a573fc5089d9f2b64a925d92c26
Xxdzs <sid.xxdzs@gmail.com>
2016-04-05 13:00:55 +0200

    Update

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/29 10:45:22 by angagnie          #+#    #+#             */
-/*   Updated: 2016/03/29 15:39:07 by angagnie         ###   ########.fr       */
+/*   Updated: 2016/04/05 12:59:17 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,41 @@
 ** -
 */
 
+t_fdsave		*get_past(t_list *save)
+{
+	t_node	*tmp;
+	fd_save	**data;
+	char	*pos;
+
+	data = &tmp;
+	tmp = save;
+	while ((tmp = tmp->next) != (t_node *)save)
+	{
+		if ((*data)->fd == fd)
+		{
+			if (pos = strchr())
+			return (*data);
+		}
+	}
+	return (NULL);
+}
+
 int		get_next_line(int const fd, char **line)
 {
 	static t_list	save;
 	t_fdsave		*past;
+	t_dyna			data;
 
 	if (*line != NULL)	// Free previous line
 	{
 		free(*line);
 		*line = NULL;
 	}
+	if ((past = get_past(fd, &save)) && handle_past(past, line))
+		return (1);
+	data = ft_dyna_new(sizeof(char));
+	ft_dyna_datainit(&data);
 
+	handle_past(fd, line,);
+	return (0);
 }
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,9 +6,12 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/29 10:45:17 by angagnie          #+#    #+#             */
-/*   Updated: 2016/03/29 15:18:31 by angagnie         ###   ########.fr       */
+/*   Updated: 2016/04/05 11:14:08 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+#include "stdlib.h"
 
 #define BUFF_SIZE 1024
 
@@ -24,3 +27,17 @@ typedef struct	s_gnl
 	char	buf[BUFF_SIZE];
 	t_dyna	acc;
 }				t_gnl;
+
+/*
+** gnl :    N x A       ->  N x S
+** |        (fd, &line) |-> (status, *line)
+** -
+** @param : fd : int            // File descriptor to read from.
+** @param : line : char **      // The address of the output string.
+** -
+** @precondition : fd should already be opened
+** @precondition : *line initial value should be NULL
+** -
+*/
+
+int				get_next_line(int const fd, char **line);
