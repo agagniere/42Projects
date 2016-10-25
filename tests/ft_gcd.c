a462ef8a41d68988460857ed7ec6cf0131cee939
Xxdzs <sid.xxdzs@gmail.com>
2016-10-25 22:39:32 +0200

    The diophantine solver is REALLY working this time\nLast commit was a mistake

--- tests/ft_gcd.c
+++ tests/ft_gcd.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/25 18:58:46 by angagnie          #+#    #+#             */
-/*   Updated: 2016/10/25 22:25:08 by angagnie         ###   ########.fr       */
+/*   Updated: 2016/10/25 22:38:30 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,6 @@ int		ft_bezout(int a, int b, int *o)
 		v[1] = v[0];
 		bool = (r[0] != 0);
 	}
-	//printf("%i * %i + %i * %i = %i\n", a, v[2], b, u[2], r[2]);
 	o[0] = v[2];
 	o[1] = u[2];
 	return (r[2]);
@@ -74,29 +73,18 @@ int		ft_diophantine(int a, int b, int c, int *o)
 	int		p;
 	int		d[2];
 
-//	printf(" ---== Solve %ix + %ib = %i ==---\n", a, b, c);
 	p = ft_bezout(a, b, d);
 	if (p == 1)
 	{
-		o[o] = c * d[0];
+		o[0] = c * d[0];
 		o[1] = c * d[1];
-//		printf(" x = %i - %ik\n", c * d[0], b);
-//		printf(" y = %i - %ik\n", c * d[1], a);
 	}
 	else
 	{
 		if (c % p == 0)
-		{
-//			printf("Multiply by %i the solutions of :\n", p);
 			ft_diophantine(a / p, b / p, c / p, o);
-			o[0] *= p;
-			o[1] *= p;
-		}
 		else
-		{
-//			ft_putstr("No solution\n");
 			return (1);
-		}
 	}
 	return (0);
 }
@@ -115,9 +103,13 @@ int		main(int ac, char **av)
 		else
 			c = 1;
 		printf("Solve :\t%ix + %iy = %i\n", a, b, c);
-		ft_diophantine(a, b, c, d);
-		printf("{ x = %i - %ik\n", d[0], b);
-		printf("{ y = %i - %ik\n", d[1], a);
+		if (!ft_diophantine(a, b, c, d))
+		{
+			printf("{ x = %i - %ik\n", d[0], b);
+			printf("{ y = %i - %ik\n", d[1], a);
+		}
+		else
+			ft_putstr("No solutions\n");
 	}
 	return (0);
 }
