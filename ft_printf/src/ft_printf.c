550ea0d3b100b70f251f8df14a1a12b7a1445942
Xxdzs <sid.xxdzs@gmail.com>
2016-10-28 17:41:04 +0200

    Better

--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: angagnie <angagnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 17:04:01 by angagnie          #+#    #+#             */
-/*   Updated: 2016/10/27 21:00:18 by angagnie         ###   ########.fr       */
+/*   Updated: 2016/10/28 17:40:41 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,67 +33,49 @@ typedef	struct	s_modifier
 	int		precision;
 }				t_modifier;
 
-typedef struct	s_progress
-{
-	char	percent;
-	char	period;
-	char	notsureaboutthisone;
-}				t_progress;
-
 #define NEW_MODIFIER (t_modifier){{{0, 0, 0, 0, 0}}, 0, 0, 0};
 
-void	tp_percent(t_modifier *m, t_progress *p)
+int		is_in(char i, char const *str)
 {
-	p->percent ^= 1;
+	char const	*p = str;
+
+	while (*p != '\0' && *p != i)
+		p++;
+	return (*p == '\0' ? -1 : p - str);
 }
 
-void	tp_period(t_modifier *m, t_progress *p)
+int		pf_match(char i, t_modifier *m)
 {
-	p->period = 1;
+	int			n;
+
+	if ((n = is_in(i, "0+- #")) >= 0)
+		m->booleans.t[n] = 1;
+	else if ((n = is_in(i, "diouxXDOUeEfFgGaAcCsSpn")) >= 0)
+	{
+		m->conversion = i;
+		return (0);
+	}
+	return (1);
 }
 
-int		pf_match(char i, t_modifier *m, t_progress *p)
-{
-	char const		*b = "0+- #";
-	char const		c[] = {'%', '.'};
-	void			*f[] = {&tp_percent, &tp_period};
-	char			*s[] = {"diouxXDOUeEfFgGaAcCsSpn", ""};
-	void			*g = {};
-	const size_t	l = 2;
-	int				n;
-	char const		*a;
-
-	a = b - 1;
-	while (*++a != '\0')
-		if (*a == i)
-		{
-			m->booleans.t[a-b] = 1;
-			return (1);
-		}
-	n = l;
-	while (n-- > 0)
-		if (c[n] == i)
-		{
-			((void (*)())f[n])(m, p);
-			return (1);
-		}
-	return (0);
-}
-
-int		pf_parse(const char *s)
+int		pf_parse(const char *s, ...)
 {
 	t_dyna		d;
 	t_modifier	m;
-	t_progress	p;
+	char		p;
 
 	d = ft_dyna_new(sizeof(char));
 	m = NEW_MODIFIER;
-	p = (t_progress){0,0,0};
+	p = 0;
 	s--;
 	while (*++s != '\0')
 	{
-		pf_match(*s, &m, &p);
-		if (!p.percent)
+		if ((p ^= (*s == '%')) && (p = pf_match(*s, &m)))
+		{
+			pf_;
+			m = NEW_MODIFIER;
+		}
+		else
 			ft_dyna_append(&d, (void *)s, 1);
 	}
 	write(1, d.data, d.chunck_count);
@@ -108,9 +90,9 @@ int		ft_printf(const char *format, ...)
 
 int		main(int ac, char **av)
 {
-	char		*s = "(%+-0%i,%d,%i)\n";
+	char		*s = "(% - + 0i)\n";
 
 	printf(s, 1, -2, 3);
-	ft_printf(s, 12, -2., 3.);
+	ft_printf(s, 2, -2, 3);
 	return (0);
 }
