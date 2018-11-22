0f0e810f71b9c7d06ff39b99c28452e8ba177021
Antoine GAGNIERE <angagnie@e3r6p18.42.fr>
2018-11-22 20:15:36 +0100

    v2

--- tests/regression.py
+++ tests/regression.py
@@ -1,3 +1,20 @@
+def avg(list):
+    return (float(sum(list)) / len(list))
+
+def apply(map, f_x, f_y):
+    return {f_x(x): f_y(y) for x,y in map.items()}
+
+def get_utils(list):
+    mean = avg(list)
+    range = max(list) - min(list)
+    print("\t{} {}".format(mean, range))
+    range /= 2.0
+    def normalize(x):
+        return ((x - mean) / range)
+    def denormalize(z):
+        return (z * range + mean)
+    return (normalize, denormalize)
+
 def linear(a, b):
     def f(x):
         return (a * x + b)
@@ -15,27 +32,41 @@ def read_csv(filename):
     return (map)
 
 def distance(map):
-    def partial(is_horizontal):
+    for (x, y) in map.items():
+        print("{:.4}, {:.4}".format(x, y))
+    def partial(single_cost):
         def total(f):
             sum = 0
             for (x,y) in map.items():
-                sum += (y - f(x)) * (x if (is_horizontal) else 1)
+                sum += single_cost(f, x, y)
             return (sum / len(map))
         return (total)
     return (partial)
 
 def train():
-    a = b = 0
     F = read_csv("data.csv")
-    dist = distance(F)
-    b_delta = dist(False)
-    a_delta = dist(True)
+    norm_mil, denorm_mil = get_utils(F.keys())
+    norm_pri, denorm_pri = get_utils(F.values())
+    delta = distance(apply(F, norm_mil, norm_pri))
+    avg_dist = delta(lambda f,x,y: y-f(x))
+    adhoc = delta(lambda f,x,y: (y-f(x))*x)
+    cost = delta(lambda f,x,y: (y-f(x))**2)
+    theta = gradient_descent([avg_dist, adhoc, cost])
+    return (denorm_pri(theta[0]), theta[1])
+
+def gradient_descent(distance_functions):
     count = 0
-    while (abs(b_delta(linear(a,b))) + abs(a_delta(linear(a,b))) > 0.001):
-        b += b_delta(linear(a, b))
-        m = max(F)
-        a += a_delta(linear(a, b)) / (m * F[m] * 3)
-        print("f(x) = {}.x + {}".format(a, b))
+    learning_rate = 1
+    theta = [0, 0]
+    prev_cost = 200
+    cur_cost = 100
+    while (count < 300 and abs(cur_cost - prev_cost) > 0.00001):
+        f = linear(theta[1], theta[0])
+        prev_cost = cur_cost
+        cur_cost = distance_functions[2](f)
+        for i in range(2):
+            theta[i] += learning_rate * distance_functions[i](f)
+        print("[{:.4}] f(x) = {:.4}.x + {:.4}".format(cur_cost, theta[1], theta[0]))
         count += 1
     print(count)
-    return (a, b)
+    return (theta)
